1ff9681b81e47499abebf2a796cfe6141ad13233
GeetaSMath <geetasmath@gmail.com>
2021-12-25 19:50:40 +0530

    Added:calculating monthly wages

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -9,25 +9,22 @@ Emp_Rate_Per_Hour = 20
 Full_Time = 8
 Part_Time = 4
 
-Emp_Check = random.randrange(0, 3)
+Working_Days = 20
+total_Emp_Hour = 0
 
-# checking attendance
-if Emp_Check == 0:
-    Emp_Hour = Full_Time
-elif Emp_Check == 1:
-     Emp_Hour = Part_Time
-else:
-    Emp_Hour = 0
+for day in range(1, Working_Days + 1):
+    Emp_Check = random.randrange(0, 3)
+
+    # checking attendance
+    if Emp_Check == 0:
+        Emp_Hour = Full_Time
+    elif Emp_Check == 1:
+        Emp_Hour = Part_Time
+    else:
+       Emp_Hour = 0
+
+    total_Emp_Hour += Emp_Hour
 
 # calculating part time PartTime
-emp_wage = Emp_Rate_Per_Hour * Emp_Hour
+emp_wage = Emp_Rate_Per_Hour * total_Emp_Hour
 print(f"Monthly Employee Wage is :{emp_wage}")
-
-
-
-
-
-
-
-
-
