1807c4f13a140803da21eff3a6ebfe8aa954204d
GeetaSMath <geetasmath@gmail.com>
2021-12-25 21:43:10 +0530

    Added:Find EmployeeWage

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,30 +1,31 @@
-"""Welcome to solve employee wage problem"""
-# imported random function
-# ---UserCase three---
-# employee Attendance
-# employee monthly wage
 import random
 
 Emp_Rate_Per_Hour = 20
-Full_Time = 8
-Part_Time = 4
+FULL_TIME = 8
+PART_TIME = 4
+WORKING_HOURS = 100
 
-Working_Days = 20
-total_Emp_Hour = 0
+Working_Days = 0
+Total_Emp_Hour = 0
 
-for day in range(1, Working_Days + 1):
-    Emp_Check = random.randrange(0, 3)
+while Working_Days < 20 and Total_Emp_Hour <= WORKING_HOURS:
+    Working_Days += 1
+    emp_check = random.randrange(0, 3)
 
-    # checking attendance
-    if Emp_Check == 0:
-        Emp_Hour = Full_Time
-    elif Emp_Check == 1:
-        Emp_Hour = Part_Time
+    # checking full-time, part-time or absent
+    if emp_check == 0:
+        emp_hour = FULL_TIME
+    elif emp_check == 1:
+        emp_hour = PART_TIME
     else:
-       Emp_Hour = 0
+        emp_hour = 0
 
-    total_Emp_Hour += Emp_Hour
+    Total_Emp_Hour += emp_hour
 
-# calculating part time PartTime
-emp_wage = Emp_Rate_Per_Hour * total_Emp_Hour
-print(f"Monthly Employee Wage is :{emp_wage}")
+    if Total_Emp_Hour > 100:
+        Total_Emp_Hour -= emp_hour
+
+
+# calculating monthly wage of employee
+emp_wage = Emp_Rate_Per_Hour * Total_Emp_Hour
+print(f"Monthly Wage of a Employee is :{emp_wage}")
